2d22012fda3f5b7346cbe57efbb64259643d955b
HC-GitH <HatimChahdi@teccart.online>
2024-09-23 15:06:48 -0400

    firstApp

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-mylist=[x for x in range(10)]
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+
+@app.get("/")
+async def welcome():
+    try:
+        return {"message": "Hello from server"}
+    except Exception as e:
+        print('Exception : {e}')
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/test")
+async def hello():
+    try:
+        return {"message": "Hello from test server"}
+    except Exception as e:
+        print('Exception : {e}')
+        raise HTTPException(status_code=500, detail=str(e))
